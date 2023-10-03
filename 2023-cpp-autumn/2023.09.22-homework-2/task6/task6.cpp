83f0ecaeb3d6a6109f4f27afe240c811e2a6a21b
veronika sergienko <st117170@student.spbu.ru>
2023-10-03 11:59:36 +0300

    minor changes

--- 2023-cpp-autumn/2023.09.22-homework-2/task6/task6.cpp
+++ 2023-cpp-autumn/2023.09.22-homework-2/task6/task6.cpp
@@ -5,24 +5,24 @@ int main()
     int k = 0;
     int m = 0;
     int n = 0;
-    int time = 0;
+    int t = 0;
     std::cin >> k;
     std::cin >> m;
     std::cin >> n;
 
-    if (k >= n)
+    if (n <= k)
     {
-        time = 2 * m;
+        t = 2 * m;
     }
-    else 
+    else
     {
-        time = (2 * n / k) * m;
-        if ((n % k != 0) && (n % k != k / 2))
+        t = (2 * (n / k)) * m;
+        if ((n % k != 0) && ((n % k) != (k / 2)))
         {
-            time += m;
+            t += m;
         }
     }
-    
-    std::cout << time;
+
+    std::cout << t;
     return EXIT_SUCCESS;
 }
