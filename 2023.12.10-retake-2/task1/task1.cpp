b0364d014e44dc29b880015b0bb10bb9490e913f
veronika sergienko <st117170@student.spbu.ru>
2023-10-12 22:01:58 +0300

    task 1 to 3 from retake-2 upload

---
+++ 2023.12.10-retake-2/task1/task1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    int result = a - b;
+    std::cout << result;
+
+    return EXIT_SUCCESS;
+    
+}
